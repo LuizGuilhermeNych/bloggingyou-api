803cbf5d7389235a7175749ce41d555744b0ce12
Luiz Guilherme Nycholas <63888721+LuizGuilhermeNych@users.noreply.github.com>
2026-09-16 17:38:07 -0300

    chore: projeto inicial gerado pelo Spring Initializr

---
+++ src/test/java/com/bloggingyou/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloggingyou.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
